463e0fdb7cc7ca840594fa3d584ceafa7c4d1862
test <umbert@gmail.com>
2021-09-17 14:28:39 +0200

    scriptw

--- .github/scripts/construct-pr-text.js
+++ .github/scripts/construct-pr-text.js
@@ -1,10 +1,9 @@
-module.exports = ({imageTag, prNumber}) => {
+module.exports = ({imageTag, prNumber, core}) => {
     const prExtra = prNumber ? '' : `from [${prNumber}](https://github.com/Unity-Technologies/live-platform/pull/${prNumber})`;
     const message = `Update OCI Image tag to ${imageTag} ${prExtra} and release to test environment`;
-    const body = 'This pull request was automatically generated from a Github Action.'
-    return {
-        'commit-message': message,
-        title: message,
-        body
-    }
+    const body = 'This pull request was automatically generated from a Github Action.';
+
+    core.setOutput('commit-message', message);
+    core.setOutput('title', message);
+    core.setOutput('body', body);
 }
