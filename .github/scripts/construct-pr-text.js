6b25058702e70cfe0b98f25b4cccc277b33c2e87
test <umbert@gmail.com>
2021-09-17 16:15:05 +0200

    scriptw

--- .github/scripts/construct-pr-text.js
+++ .github/scripts/construct-pr-text.js
@@ -4,10 +4,17 @@ module.exports = (imageTag, prNumber, rawTitle, rawBody, { core }) => {
     var body = 'This pull request was automatically generated from a Github Action.';
 
     if (prNumber) {
+        const linear = rawTitle.match("LP-([0-9]*)") ?? rawBody.match("LP-([0-9]*)")
+
+        let linearLink = ''
+        if (linear) {
+            linearLink = `Spawned from the task: [${linear[0]}](https://linear.app/unity3d/issue/${linear[0]})`
+        }
+
         title = `Update OCI Image tag to ${imageTag} from PR#${prNumber} and release to test environment`;
         message = `Update OCI Image tag to ${imageTag} from PR#${prNumber} and release to test environment`;
         body = 'This pull request was automatically generated from a Github Action.\n' +
-            `The PR [${prNumber}](https://github.com/Unity-Technologies/live-platform/pull/${prNumber}) is the origin for this changes\n` +
+            `${linearLink}The PR [${prNumber}](https://github.com/Unity-Technologies/live-platform/pull/${prNumber}) is the origin for this changes\n` +
         `<details> <summary> ${rawTitle} </summary> ${rawBody} </details>`;
     }
 
